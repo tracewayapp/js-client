6bb21b21c14fd26b85f823efe3656c500fca758d
ddux <dusan.stanojevic.cs@gmail.com>
2026-03-27 21:28:17 -0500

    progress on cdn deploy

--- packages/jquery/tsup.config.ts
+++ packages/jquery/tsup.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  clean: true,
-  sourcemap: true,
-  platform: "browser",
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    platform: "browser",
+  },
+  {
+    entry: { "traceway-jquery.iife": "src/index.ts" },
+    format: ["iife"],
+    globalName: "TracewayJQuery",
+    platform: "browser",
+    noExternal: [/.*/],
+    sourcemap: true,
+    minify: true,
+  },
+]);
